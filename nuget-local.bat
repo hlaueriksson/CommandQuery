70aa58630b60e7f90292ca41349a7b9c0e732769
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2018-07-06 23:09:15 +0200

    Add script for local nuget feed

---
+++ nuget-local.bat
@@ -0,0 +1,3 @@
+nuget init .\src\CommandQuery\bin\Release\ .\packages
+nuget init .\src\CommandQuery.AspNetCore\bin\Release\ .\packages
+nuget init .\src\CommandQuery.AzureFunctions\bin\Release\ .\packages
