5dc93b3dac8dc770cf7187a63b818a59c899376c
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2018-09-12 21:24:32 +0200

    AWSLambda sample specs

---
+++ sample/CommandQuery.Sample.Specs/AWSLambda/FakeLambdaContext.cs
@@ -0,0 +1,38 @@
+﻿#if NETCOREAPP2_0
+using System;
+using Amazon.Lambda.Core;
+
+namespace CommandQuery.Sample.Specs.AWSLambda
+{
+    public class FakeLambdaContext : ILambdaContext
+    {
+        public string AwsRequestId { get; set; }
+        public IClientContext ClientContext { get; set; }
+        public string FunctionName { get; set; }
+        public string FunctionVersion { get; set; }
+        public ICognitoIdentity Identity { get; set; }
+        public string InvokedFunctionArn { get; set; }
+        public ILambdaLogger Logger { get; set; }
+        public string LogGroupName { get; set; }
+        public string LogStreamName { get; set; }
+        public int MemoryLimitInMB { get; set; }
+        public TimeSpan RemainingTime { get; set; }
+
+        public FakeLambdaContext()
+        {
+            Logger = new FakeLambdaLogger();
+        }
+    }
+
+    public class FakeLambdaLogger : ILambdaLogger
+    {
+        public void Log(string message)
+        {
+        }
+
+        public void LogLine(string message)
+        {
+        }
+    }
+}
+#endif
---
+++ sample/CommandQuery.Sample.Specs/AWSLambda/SpecsExtensions.cs
@@ -0,0 +1,30 @@
+﻿#if NETCOREAPP2_0
+using System.Collections.Generic;
+using Amazon.Lambda.APIGatewayEvents;
+using Newtonsoft.Json;
+
+namespace CommandQuery.Sample.Specs.AWSLambda
+{
+    public static class SpecsExtensions
+    {
+        public static APIGatewayProxyRequest QueryName(this APIGatewayProxyRequest request, string queryName)
+        {
+            request.PathParameters = new Dictionary<string, string> { { "queryName", queryName } };
+
+            return request;
+        }
+
+        public static APIGatewayProxyRequest CommandName(this APIGatewayProxyRequest request, string commandName)
+        {
+            request.PathParameters = new Dictionary<string, string> { { "commandName", commandName } };
+
+            return request;
+        }
+
+        public static T As<T>(this APIGatewayProxyResponse result)
+        {
+            return JsonConvert.DeserializeObject<T>(result.Body);
+        }
+    }
+}
+#endif
