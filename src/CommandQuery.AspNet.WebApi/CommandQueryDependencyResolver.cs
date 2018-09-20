a1350d861f259845372aee2afce61cf8628374c9
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2018-09-20 21:39:18 +0200

    CommandQueryDependencyResolver

---
+++ src/CommandQuery.AspNet.WebApi/CommandQueryDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Web.Http.Dependencies;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandQuery.AspNet.WebApi
+{
+    public class CommandQueryDependencyResolver : IDependencyResolver
+    {
+        private readonly IServiceProvider _provider;
+
+        public CommandQueryDependencyResolver(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public CommandQueryDependencyResolver(IServiceCollection services)
+        {
+            services.AddServiceProvider();
+            services.AddControllers(Assembly.GetCallingAssembly());
+
+            _provider = services.BuildServiceProvider();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _provider.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _provider.GetServices(serviceType);
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new CommandQueryDependencyResolver(_provider.CreateScope().ServiceProvider);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
--- src/CommandQuery.AspNet.WebApi/ServiceCollectionExtensions.cs
+++ src/CommandQuery.AspNet.WebApi/ServiceCollectionExtensions.cs
@@ -2,12 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Web.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CommandQuery.AspNet.WebApi
 {
     public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddServiceProvider(this IServiceCollection services)
+        {
+            services.AddTransient<IServiceProvider>(_ => services.BuildServiceProvider());
+
+            return services;
+        }
+
+        public static IServiceCollection AddControllers(this IServiceCollection services, Assembly assembly = null)
+        {
+            var controllerTypes = (assembly ?? Assembly.GetCallingAssembly()).GetExportedTypes()
+                .Where(t => !t.IsAbstract && !t.IsGenericTypeDefinition)
+                .Where(t => typeof(ApiController).IsAssignableFrom(t) || t.Name.EndsWith("Controller", StringComparison.OrdinalIgnoreCase));
+            foreach (var type in controllerTypes)
+            {
+                services.AddTransient(type);
+            }
+
+            return services;
+        }
+
         public static IServiceCollection AddCommands(this IServiceCollection services, params Assembly[] assemblies)
         {
             var genericType = typeof(ICommandHandler<>);
