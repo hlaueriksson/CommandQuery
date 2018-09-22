6a6c5e09fd79357c4baa54671dd829cb1e01e5de
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2018-09-22 23:13:30 +0200

    nuget script

--- nuget-local.bat
+++ nuget-local.bat
@@ -1,4 +1,5 @@
 nuget init .\src\CommandQuery\bin\Release\ .\packages
 nuget init .\src\CommandQuery.AspNet.WebApi\bin\Release\ .\packages
 nuget init .\src\CommandQuery.AspNetCore\bin\Release\ .\packages
+nuget init .\src\CommandQuery.AWSLambda\bin\Release\ .\packages
 nuget init .\src\CommandQuery.AzureFunctions\bin\Release\ .\packages
