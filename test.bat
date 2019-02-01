8fdbc1583156e848c8e0200b25cc8b0dea312aad
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-02-01 22:33:42 +0100

    :truck: tests

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
-dotnet test .\test\CommandQuery.Specs\CommandQuery.Specs.csproj
+dotnet test .\tests\CommandQuery.Specs\CommandQuery.Specs.csproj
 dotnet test .\samples\CommandQuery.Sample.Specs\CommandQuery.Sample.Specs.csproj
