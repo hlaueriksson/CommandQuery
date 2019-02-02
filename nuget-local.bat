bbafc62bd68d065e447996fbb18839822bd94024
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-02-02 23:55:36 +0100

    pack

--- nuget-local.bat
+++ nuget-local.bat
@@ -1,5 +1,7 @@
 nuget init .\src\CommandQuery\bin\Release\ .\packages
+nuget init .\src\CommandQuery.Abstractions\bin\Release\ .\packages
 nuget init .\src\CommandQuery.AspNet.WebApi\bin\Release\ .\packages
 nuget init .\src\CommandQuery.AspNetCore\bin\Release\ .\packages
 nuget init .\src\CommandQuery.AWSLambda\bin\Release\ .\packages
 nuget init .\src\CommandQuery.AzureFunctions\bin\Release\ .\packages
+nuget init .\src\CommandQuery.Client\bin\Release\ .\packages
