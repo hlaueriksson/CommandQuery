9cffbedc1527e42d4d36d23db2b9c577401f07dd
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-11-17 00:48:16 +0100

    pack

--- pack.bat
+++ pack.bat
@@ -1,7 +1,7 @@
-dotnet build -c Release
+dotnet build CommandQuery.sln -c Release
 
 :: Fix for extra bin folder
 xcopy /y .\src\CommandQuery.AzureFunctions\bin\Release\net461\bin\CommandQuery.AzureFunctions.* .\src\CommandQuery.AzureFunctions\bin\Release\net461\
 xcopy /y .\src\CommandQuery.AzureFunctions\bin\Release\netstandard2.0\bin\CommandQuery.AzureFunctions.* .\src\CommandQuery.AzureFunctions\bin\Release\netstandard2.0\
 
-dotnet pack -c Release --no-build
+dotnet pack CommandQuery.sln --no-build -c Release
