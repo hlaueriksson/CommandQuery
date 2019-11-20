702fb6880207953c006c6121ee56ed7b5bcc5508
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-11-20 22:05:27 +0100

    fix failing ci test

--- tests/CommandQuery.Tests/Client/InternalExtensionsTests.cs
+++ tests/CommandQuery.Tests/Client/InternalExtensionsTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
 using CommandQuery.Client;
 using FluentAssertions;
 using LoFuUnit.NUnit;
@@ -12,6 +14,8 @@ namespace CommandQuery.Tests.Client
         [LoFu, Test]
         public void when_converting_a_dictionary_to_object()
         {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
             var subject = new FakeComplexQuery
             {
                 String = "String",
@@ -31,9 +35,9 @@ namespace CommandQuery.Tests.Client
                 .Contain("String=String").And
                 .Contain("Int=1").And
                 .Contain("Bool=True").And
-                .Contain("DateTime=2019-11-19+17%3A50%3A13").And
+                .Contain("DateTime=11%2F19%2F2019+5%3A50%3A13+PM").And
                 .Contain("Guid=b665da2a-60fe-4f2b-baf1-9d766e2542d3").And
-                .Contain("NullableDouble=2%2C1").And
+                .Contain("NullableDouble=2.1").And
                 .Contain("Array=1&Array=2").And
                 .Contain("IEnumerable=3&IEnumerable=4").And
                 .Contain("List=5&List=6");
