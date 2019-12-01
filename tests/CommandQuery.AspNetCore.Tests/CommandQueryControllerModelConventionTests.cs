e2ae064f08b72c7b4823f140f1724d4ecbc4c274
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-12-01 23:02:20 +0100

    tests

---
+++ tests/CommandQuery.AspNetCore.Tests/CommandQueryControllerModelConventionTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using CommandQuery.Tests;
+using FluentAssertions;
+using LoFuUnit.NUnit;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using NUnit.Framework;
+
+namespace CommandQuery.AspNetCore.Tests
+{
+    public class CommandQueryControllerModelConventionTests
+    {
+        [LoFu, Test]
+        public async Task when_Apply()
+        {
+            Subject = new CommandQueryControllerModelConvention();
+
+            void should_handle_CommandControllers()
+            {
+                var result = new ControllerModel(typeof(CommandController<FakeCommand>).GetTypeInfo(), new List<object>());
+                Subject.Apply(result);
+                result.ControllerName.Should().Be("FakeCommand");
+            }
+
+            void should_handle_CommandWithResultControllers()
+            {
+                var result = new ControllerModel(typeof(CommandWithResultController<FakeResultCommand, FakeResult>).GetTypeInfo(), new List<object>());
+                Subject.Apply(result);
+                result.ControllerName.Should().Be("FakeResultCommand");
+            }
+
+            void should_handle_QueryControllers()
+            {
+                var result = new ControllerModel(typeof(QueryController<FakeQuery, FakeResult>).GetTypeInfo(), new List<object>());
+                Subject.Apply(result);
+                result.ControllerName.Should().Be("FakeQuery");
+            }
+        }
+
+        CommandQueryControllerModelConvention Subject;
+    }
+}
---
+++ tests/CommandQuery.AspNetCore.Tests/QueryControllerFeatureProviderTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using CommandQuery.Tests;
+using FluentAssertions;
+using LoFuUnit.NUnit;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using NUnit.Framework;
+
+namespace CommandQuery.AspNetCore.Tests
+{
+    public class QueryControllerFeatureProviderTests
+    {
+        [LoFu, Test]
+        public async Task when_PopulateFeature()
+        {
+            var subject = new QueryControllerFeatureProvider(typeof(FakeQuery).Assembly);
+            Result = new ControllerFeature();
+            subject.PopulateFeature(null, Result);
+
+            void should_add_QueryControllers() =>
+                Result.Controllers.Should().Contain(typeof(QueryController<FakeQuery, FakeResult>).GetTypeInfo());
+        }
+
+        ControllerFeature Result;
+    }
+}
