a995e00c42f94c88493caad149aece0f07f34a35
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-12-15 21:21:44 +0100

    test

--- test.bat
+++ test.bat
@@ -4,8 +4,10 @@ dotnet test .\tests\CommandQuery.AWSLambda.Tests\CommandQuery.AWSLambda.Tests.cs
 dotnet test .\tests\CommandQuery.AzureFunctions.Tests\CommandQuery.AzureFunctions.Tests.csproj
 dotnet test .\tests\CommandQuery.Tests\CommandQuery.Tests.csproj
 
+:: dotnet test .\samples\CommandQuery.Sample.AspNet.WebApi.Tests\CommandQuery.Sample.AspNet.WebApi.Tests.csproj
 dotnet test .\samples\CommandQuery.Sample.AspNetCore.V2.Tests\CommandQuery.Sample.AspNetCore.V2.Tests.csproj
 dotnet test .\samples\CommandQuery.Sample.AspNetCore.V3.Tests\CommandQuery.Sample.AspNetCore.V3.Tests.csproj
 dotnet test .\samples\CommandQuery.Sample.AWSLambda.Tests\CommandQuery.Sample.AWSLambda.Tests.csproj
 dotnet test .\samples\CommandQuery.Sample.AzureFunctions.Vs1.Tests\CommandQuery.Sample.AzureFunctions.Vs1.Tests.csproj
 dotnet test .\samples\CommandQuery.Sample.AzureFunctions.Vs2.Tests\CommandQuery.Sample.AzureFunctions.Vs2.Tests.csproj
+dotnet test .\samples\CommandQuery.Sample.AzureFunctions.Vs3.Tests\CommandQuery.Sample.AzureFunctions.Vs3.Tests.csproj
