d8c55583160cc3f0704882074d2af6dce12f9013
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2020-02-02 16:58:14 +0100

    docs

--- src/CommandQuery.Client/CommandQueryException.cs
+++ src/CommandQuery.Client/CommandQueryException.cs
@@ -2,10 +2,21 @@
 
 namespace CommandQuery.Client
 {
+    /// <summary>
+    /// Represents errors that occur when receiving HTTP responses in the <see cref="CommandClient" /> and <see cref="QueryClient" />.
+    /// </summary>
     public class CommandQueryException : Exception
     {
+        /// <summary>
+        /// Represents an error that occured during the processing of a command or query.
+        /// </summary>
         public Error Error { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandQueryException" /> class with a specified error message and <see cref="CommandQuery.Error" />.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="error"></param>
         public CommandQueryException(string message, Error error) : base(message)
         {
             Error = error;
