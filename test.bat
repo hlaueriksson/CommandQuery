b2cc652e206117213675c5e24616b246f09a0f4c
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-07-13 23:23:24 +0200

    Scripts

--- test.bat
+++ test.bat
@@ -1,13 +1,5 @@
-dotnet test .\tests\CommandQuery.AspNet.WebApi.Tests\CommandQuery.AspNet.WebApi.Tests.csproj
 dotnet test .\tests\CommandQuery.AspNetCore.Tests\CommandQuery.AspNetCore.Tests.csproj
 dotnet test .\tests\CommandQuery.AWSLambda.Tests\CommandQuery.AWSLambda.Tests.csproj
 dotnet test .\tests\CommandQuery.AzureFunctions.Tests\CommandQuery.AzureFunctions.Tests.csproj
+dotnet test .\tests\CommandQuery.GoogleCloudFunctions.Tests\CommandQuery.GoogleCloudFunctions.Tests.csproj
 dotnet test .\tests\CommandQuery.Tests\CommandQuery.Tests.csproj
-
-:: dotnet test .\samples\CommandQuery.Sample.AspNet.WebApi.Tests\CommandQuery.Sample.AspNet.WebApi.Tests.csproj
-dotnet test .\samples\CommandQuery.Sample.AspNetCore.V2.Tests\CommandQuery.Sample.AspNetCore.V2.Tests.csproj
-dotnet test .\samples\CommandQuery.Sample.AspNetCore.V3.Tests\CommandQuery.Sample.AspNetCore.V3.Tests.csproj
-dotnet test .\samples\CommandQuery.Sample.AWSLambda.Tests\CommandQuery.Sample.AWSLambda.Tests.csproj
-dotnet test .\samples\CommandQuery.Sample.AzureFunctions.Vs1.Tests\CommandQuery.Sample.AzureFunctions.Vs1.Tests.csproj
-dotnet test .\samples\CommandQuery.Sample.AzureFunctions.Vs2.Tests\CommandQuery.Sample.AzureFunctions.Vs2.Tests.csproj
-dotnet test .\samples\CommandQuery.Sample.AzureFunctions.Vs3.Tests\CommandQuery.Sample.AzureFunctions.Vs3.Tests.csproj
