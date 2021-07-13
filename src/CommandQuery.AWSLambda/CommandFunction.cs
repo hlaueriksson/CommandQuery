01db0e4e0813d035caadb3ecb5e121bd9dcb7474
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-07-13 22:01:41 +0200

    Add interfaces for CommandQuery.AWSLambda
    AWS Lambda may get Dependency Injection in the future

--- src/CommandQuery.AWSLambda/CommandFunction.cs
+++ src/CommandQuery.AWSLambda/CommandFunction.cs
@@ -11,7 +11,7 @@ namespace CommandQuery.AWSLambda
     /// <summary>
     /// Handles commands for the Lambda function.
     /// </summary>
-    public class CommandFunction
+    public class CommandFunction : ICommandFunction
     {
         private readonly ICommandProcessor _commandProcessor;
         private readonly JsonSerializerOptions? _options;
@@ -27,14 +27,7 @@ namespace CommandQuery.AWSLambda
             _options = options;
         }
 
-        /// <summary>
-        /// Handle a command.
-        /// </summary>
-        /// <param name="commandName">The name of the command.</param>
-        /// <param name="request">An <see cref="APIGatewayProxyRequest"/>.</param>
-        /// <param name="logger">An <see cref="ILambdaLogger"/>.</param>
-        /// <returns>The result for status code <c>200</c>, or an error for status code <c>400</c> and <c>500</c>.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="request"/> is <see langword="null"/>.</exception>
+        /// <inheritdoc />
         public async Task<APIGatewayProxyResponse> HandleAsync(string commandName, APIGatewayProxyRequest request, ILambdaLogger? logger)
         {
             logger?.LogLine($"Handle {commandName}");
--- src/CommandQuery.AWSLambda/QueryFunction.cs
+++ src/CommandQuery.AWSLambda/QueryFunction.cs
@@ -12,7 +12,7 @@ namespace CommandQuery.AWSLambda
     /// <summary>
     /// Handles queries for the Lambda function.
     /// </summary>
-    public class QueryFunction
+    public class QueryFunction : IQueryFunction
     {
         private readonly IQueryProcessor _queryProcessor;
         private readonly JsonSerializerOptions? _options;
@@ -28,14 +28,7 @@ namespace CommandQuery.AWSLambda
             _options = options;
         }
 
-        /// <summary>
-        /// Handle a query.
-        /// </summary>
-        /// <param name="queryName">The name of the query.</param>
-        /// <param name="request">An <see cref="APIGatewayProxyRequest"/>.</param>
-        /// <param name="logger">An <see cref="ILambdaLogger"/>.</param>
-        /// <returns>The result for status code <c>200</c>, or an error for status code <c>400</c> and <c>500</c>.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="request"/> is <see langword="null"/>.</exception>
+        /// <inheritdoc />
         public async Task<APIGatewayProxyResponse> HandleAsync(string queryName, APIGatewayProxyRequest request, ILambdaLogger? logger)
         {
             logger?.LogLine($"Handle {queryName}");
