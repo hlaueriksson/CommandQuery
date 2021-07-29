7acc7771ad4e0a3ab2774613d9991e269fd21748
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-07-29 10:56:58 +0200

    Fix deploy script for CommandQuery.Sample.GoogleCloudFunctions

--- samples/CommandQuery.Sample.GoogleCloudFunctions/deploy.bat
+++ samples/CommandQuery.Sample.GoogleCloudFunctions/deploy.bat
@@ -1,2 +1,6 @@
-gcloud functions deploy commandquery-sample-googlecloudfunctions-command --runtime dotnet3 --trigger-http --entry-point CommandQuery.Sample.GoogleCloudFunctions.Command
-gcloud functions deploy commandquery-sample-googlecloudfunctions-query --runtime dotnet3 --trigger-http --entry-point CommandQuery.Sample.GoogleCloudFunctions.Query
+cd ..
+
+CMD /C gcloud functions deploy commandquery-sample-googlecloudfunctions-command --runtime dotnet3 --trigger-http --entry-point=CommandQuery.Sample.GoogleCloudFunctions.Command --set-build-env-vars=GOOGLE_BUILDABLE=CommandQuery.Sample.GoogleCloudFunctions
+CMD /C gcloud functions deploy commandquery-sample-googlecloudfunctions-query --runtime dotnet3 --trigger-http --entry-point=CommandQuery.Sample.GoogleCloudFunctions.Query --set-build-env-vars=GOOGLE_BUILDABLE=CommandQuery.Sample.GoogleCloudFunctions
+
+cd CommandQuery.Sample.GoogleCloudFunctions
