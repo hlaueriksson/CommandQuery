64d163adcdabb363c5f4a57a42eafd7fe0eb9acc
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2024-07-13 20:00:39 +0200

    gcp

--- samples/CommandQuery.Sample.Client/run.bat
+++ samples/CommandQuery.Sample.Client/run.bat
@@ -1,4 +1,4 @@
 dotnet run https://commandquerysampleaspnetcore.azurewebsites.net/api
 dotnet run https://u3rkew3obh.execute-api.eu-central-1.amazonaws.com/Prod
 dotnet run https://commandquerysampleazurefunctions.azurewebsites.net/api
-dotnet run https://us-central1-solid-antler-215713.cloudfunctions.net/commandquery-sample-googlecloudfunctions-command/api/command/ https://us-central1-solid-antler-215713.cloudfunctions.net/commandquery-sample-googlecloudfunctions-query/api/query/
+dotnet run https://europe-north1-solid-antler-215713.cloudfunctions.net/commandquery-sample-googlecloudfunctions-command/api/command/ https://europe-north1-solid-antler-215713.cloudfunctions.net/commandquery-sample-googlecloudfunctions-query/api/query/
--- samples/CommandQuery.Sample.GoogleCloudFunctions/deploy.bat
+++ samples/CommandQuery.Sample.GoogleCloudFunctions/deploy.bat
@@ -1,6 +1,7 @@
-cd ..
+:: Deploy from the root directory
+cd ..\..
 
-CMD /C gcloud functions deploy commandquery-sample-googlecloudfunctions-command --runtime dotnet8 --trigger-http --entry-point=CommandQuery.Sample.GoogleCloudFunctions.Command --set-build-env-vars=GOOGLE_BUILDABLE=CommandQuery.Sample.GoogleCloudFunctions
-CMD /C gcloud functions deploy commandquery-sample-googlecloudfunctions-query --runtime dotnet8 --trigger-http --entry-point=CommandQuery.Sample.GoogleCloudFunctions.Query --set-build-env-vars=GOOGLE_BUILDABLE=CommandQuery.Sample.GoogleCloudFunctions
+CMD /C gcloud functions deploy commandquery-sample-googlecloudfunctions-command --gen2 --region=europe-north1 --runtime=dotnet8 --trigger-http --entry-point=CommandQuery.Sample.GoogleCloudFunctions.Command --set-build-env-vars=GOOGLE_BUILDABLE=samples/CommandQuery.Sample.GoogleCloudFunctions
+CMD /C gcloud functions deploy commandquery-sample-googlecloudfunctions-query --gen2 --region=europe-north1 --runtime=dotnet8 --trigger-http --entry-point=CommandQuery.Sample.GoogleCloudFunctions.Query --set-build-env-vars=GOOGLE_BUILDABLE=samples/CommandQuery.Sample.GoogleCloudFunctions
 
-cd CommandQuery.Sample.GoogleCloudFunctions
+cd samples\CommandQuery.Sample.GoogleCloudFunctions
